ddbb42f3271d15982e55bfb5ae43d1619f8e7f0a
Carlos Esteven <carlos.cancimance00@usc.edu.co>
2022-07-12 16:43:54 -0500

    update code

--- contratos/3_Dex_Token.sol
+++ contratos/3_Dex_Token.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 interface IERC20 {
-
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function allowance(address owner, address spender) external view returns (uint256);
@@ -10,7 +9,6 @@ interface IERC20 {
     function transfer(address recipient, uint256 amount) external returns (bool);
     function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
@@ -69,4 +67,36 @@ contract Token_ERC20 is IERC20 {
         emit Transfer(owner, buyer, numTokens);
         return true;
     }
+}
+
+
+contract DEX {
+
+    event Bought(uint256 amount);
+    event Sold(uint256 amount);
+
+    IERC20 public token;
+
+    constructor() {
+        token = new Token_ERC20();
+    }
+
+    function buy() payable public {
+        uint256 amountTobuy = msg.value;
+        uint256 dexBalance = token.balanceOf(address(this));
+        require(amountTobuy > 0, "You need to send some ether");
+        require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
+        token.transfer(msg.sender, amountTobuy);
+        emit Bought(amountTobuy);
+    }
+
+    function sell(uint256 amount) public {
+        require(amount > 0, "You need to sell at least some tokens");
+        uint256 allowance = token.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Check the token allowance");
+        token.transferFrom(msg.sender, address(this), amount);
+        payable(msg.sender).transfer(amount);
+        emit Sold(amount);
+    }
+
 }
